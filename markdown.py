69d7591b2b72486050752668512974b28908bffa
Julien <julien.mousqueton@computacenter.com>
2022-06-05 21:14:39 +0200

    typo

--- markdown.py
+++ markdown.py
@@ -87,7 +87,7 @@ def mainpage():
     writeline(uptime_sheet, '')
     writeline(uptime_sheet, '')
     writeline(uptime_sheet, '')
-    writeline(uptime_sheet, '_Last update : '+ time.strftime('%A %d/%m/%Y %H:%M:%S') + ' (UTC)_')
+    writeline(uptime_sheet, 'Last update : _'+ time.strftime('%A %d/%m/%Y %H:%M:%S') + ' (UTC)_')
     writeline(uptime_sheet, '')
 
 def indexpage():
