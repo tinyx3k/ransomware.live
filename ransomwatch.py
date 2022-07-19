fb72f3cc1a6008e3f5720c445148713d87c3388d
Julien Mousqueton <julien@mousqueton.io>
2022-07-19 19:06:10 +0200

    Update ransomwatch.py

--- ransomwatch.py
+++ ransomwatch.py
@@ -13,10 +13,10 @@ from datetime import datetime
 # local imports
 
 import parsers
+import geckodrive
 #########################################################
-# import geckodrive
 # ref https://github.com/joshhighet/ransomwatch/issues/22
-import chromium as geckodrive
+# import chromium as geckodrive
 #########################################################
 from markdown import main as markdown
 
@@ -279,3 +279,4 @@ if args.mode == 'parse':
 
 if args.mode == 'list':
     lister()
+ 
