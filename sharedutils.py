0feb7c28a9a8b01fc7da35876435002e4751c18c
Julien <julien.mousqueton@computacenter.com>
2022-09-01 09:56:48 +0200

    indentation error

--- sharedutils.py
+++ sharedutils.py
@@ -356,7 +356,7 @@ def monthlypostcount():
     for post in posts:
         datetime_object = datetime.strptime(post['discovered'], '%Y-%m-%d %H:%M:%S.%f')
         if datetime_object.year == current_year and datetime_object.month == current_month:
-                post_count += 1
+            post_count += 1
     return post_count
 
 def postssince(days):
