a5686afb4216b7fb5f4c233bc5597c2dc4e25990
Julien <julien.mousqueton@computacenter.com>
2022-11-01 16:07:02 +0100

    Add shaoleaks

--- ransomwatch.py
+++ ransomwatch.py
@@ -281,6 +281,7 @@ if args.mode == 'parse':
     parsers.donutleaks()
     parsers.sparta()
     parsers.qilin()
+    parsers.shaoleaks()
     stdlog('ransomwatch: ' + 'parse run complete')
 
 if args.mode == 'list':
