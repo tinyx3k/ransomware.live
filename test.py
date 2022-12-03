4b1d26d1c03da8e199810227a81c4bbbbb6ef355
Julien <julien.mousqueton@computacenter.com>
2022-12-03 13:27:31 +0100

    remove test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-
-
-with open("source/play-mbrlkbtq5jonaqkurjwmxftytyn2ethqvbxfu4rgjbkkknndqwae6byd.html") as fp:
-    soup = BeautifulSoup(fp, "html.parser")
-    print(soup.prettify())
-
-
