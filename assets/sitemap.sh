0421ffcb9e1e5af1bbe603ca056452ff9fd44bdc
Julien <julien.mousqueton@computacenter.com>
2022-12-11 16:34:18 +0100

    Update siteamp

--- assets/sitemap.sh
+++ assets/sitemap.sh
@@ -9,21 +9,12 @@ FREQ="weekly"
 # begin new sitemap
 exec 1> docs/sitemap.xml
 
+DATE=$(date +%F)
+
 # print head
 echo '<?xml version="1.0" encoding="UTF-8"?>'
 echo '<!-- generator="Julien Mousqueton Sitemap Generator" -->'
 echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
-
-# print urls
-for GROUP in $(grep name groups.json  | cut -d: -f2  | tr -d , | tr -d \")
-do 
-  DATE=$(date +%F)
-  echo "<url>"
-  echo " <loc>${URL}${GROUP}</loc>"
-  echo " <lastmod>$DATE</lastmod>"
-  echo " <changefreq>$FREQ</changefreq>"
-  echo "</url>"
-done
 
 
 echo "<url>"
@@ -31,6 +22,46 @@ echo "<url>"
   echo " <lastmod>$DATE</lastmod>"
   echo " <changefreq>$FREQ</changefreq>"
   echo "</url>"
+
+echo "<url>"
+  echo " <loc>https://www.ransomware.live/#/allposts</loc>"
+  echo " <lastmod>$DATE</lastmod>"
+  echo " <changefreq>$FREQ</changefreq>"
+  echo "</url>"
+
+echo "<url>"
+  echo " <loc>https://www.ransomware.live/#/about</loc>"
+  echo " <lastmod>$DATE</lastmod>"
+  echo " <changefreq>$FREQ</changefreq>"
+  echo "</url>"
+
+echo "<url>"
+  echo " <loc>https://www.ransomware.live/#/INDEX</loc>"
+  echo " <lastmod>$DATE</lastmod>"
+  echo " <changefreq>$FREQ</changefreq>"
+  echo "</url>"
+
+echo "<url>"
+  echo " <loc>https://www.ransomware.live/#/stats</loc>"
+  echo " <lastmod>$DATE</lastmod>"
+  echo " <changefreq>$FREQ</changefreq>"
+  echo "</url>"
+
+echo "<url>"
+  echo " <loc>https://www.ransomware.live/#/profiles</loc>"
+  echo " <lastmod>$DATE</lastmod>"
+  echo " <changefreq>$FREQ</changefreq>"
+  echo "</url>"
+
+# print urls
+for GROUP in $(grep name groups.json  | cut -d: -f2  | tr -d , | tr -d \")
+do 
+  echo "<url>"
+  echo " <loc>${URL}${GROUP}</loc>"
+  echo " <lastmod>$DATE</lastmod>"
+  echo " <changefreq>$FREQ</changefreq>"
+  echo "</url>"
+done
 
 # print foot
 echo "</urlset>"
