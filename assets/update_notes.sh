369f2fdfb6792161ef71a411ec67b72f6849489d
Julien <julien.mousqueton@computacenter.com>
2022-12-19 12:54:29 +0100

    remove unnecessary file

--- assets/update_notes.sh
+++
@@ -1,6 +0,0 @@
-cd ./assets/tmp
-git clone https://github.com/threatlabz/ransomware_notes
-cd ransomware_notes
-cp -R * ../../../docs/ransomware_notes
-cd ../../..
-rm -Rf ./assets/tmp/ransomware_notes
