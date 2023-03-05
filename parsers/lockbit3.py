95e7f766aea4670c2f50300d15ba572406104f77
Julien <julien.mousqueton@computacenter.com>
2023-03-05 15:29:52 +0100

    Update Lockbit3 Parser with published date

--- parsers/lockbit3.py
+++ parsers/lockbit3.py
@@ -1,7 +1,17 @@
+"""
++------------------------------+------------------+----------+
+| Description | Published Date | Victim's Website | Post URL |
++------------------------------+------------------+----------+
+|      X      |        X       |                  |          |
++------------------------------+------------------+----------+
+Rappel : def appender(post_title, group_name, description="", website="", published="", post_url=""):
+"""
+
 import os
 from bs4 import BeautifulSoup
 from sharedutils import errlog
 import parse
+from datetime import datetime
 
 def main():
     for filename in os.listdir('source'):
@@ -14,12 +24,18 @@ def main():
                 for div in divs_name:
                     title = div.find('div',{"class": "post-title"}).text.strip()
                     description = div.find('div',{"class" : "post-block-text"}).text.strip()
-                    parse.appender(title, 'lockbit3', description.replace('\n',''))
+                    published = div.find('div',{"class" : "updated-post-date"}).text.strip()
+                    date_obj = datetime.strptime(published.replace('Updated: ',''), "%d %b, %Y,\xa0\xa0 %H:%M %Z")
+                    published = date_obj.strftime("%Y-%m-%d %H:%M:%S.%f")
+                    parse.appender(title, 'lockbit3', description.replace('\n',''),"",published)
                 divs_name=soup.find_all('div', {"class": "post-block good"})
                 for div in divs_name:
                     title = div.find('div',{"class": "post-title"}).text.strip()
                     description = div.find('div',{"class" : "post-block-text"}).text.strip()
-                    parse.appender(title, 'lockbit3', description.replace('\n',''))
+                    published = div.find('div',{"class" : "updated-post-date"}).text.strip()
+                    date_obj = datetime.strptime(published.replace('Updated: ',''), "%d %b, %Y,\xa0\xa0 %H:%M %Z")
+                    published = date_obj.strftime("%Y-%m-%d %H:%M:%S.%f")
+                    parse.appender(title, 'lockbit3', description.replace('\n',''),"",published)
                 file.close()
         except:
             errlog('lockbit3: ' + 'parsing fail')
